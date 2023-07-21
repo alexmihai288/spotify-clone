947919a66e02ab09d713b821f8c94511d4d99441
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2023-07-21 10:53:27 +0300

    project

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+import { ProductWithPrice } from "@/types";
+import { useEffect, useState } from "react";
+
+interface ModalProviderProps{
+}
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+
+    if(!isMounted){
+        return null;
+    }
+
+  return (
+    <>
+      <AuthModal/>
+      <UploadModal/>
+    </>
+  )
+};
+
+export default ModalProvider
